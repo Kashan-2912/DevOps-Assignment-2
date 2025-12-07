703c421227e2e461b3cc95cba142d3721851a390
Muhammad Kashan Ashraf <168653347+Kashan-2912@users.noreply.github.com>
2025-12-07 17:13:17 +0500

    Update API base URL for production environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3001/api":
-      "http://3.110.105.30:3001/api",
+      "http://13.234.238.153:5174/api",
       // : "http://ezyshopper-env.eba-x2zz233e.ap-south-1.elasticbeanstalk.com/api",
   withCredentials: true,
 });
